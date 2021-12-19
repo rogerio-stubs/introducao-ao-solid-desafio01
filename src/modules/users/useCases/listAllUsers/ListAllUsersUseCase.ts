96d3a4d9df487aef3bfeebf3bb9399af7a70479f
rogerio-stubs <rostubs17@gmail.com>
2021-12-19 03:47:05 -0300

    desafio 01 - introduçao ao SOLID

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    if (!userAdmin.admin) {
+      throw new Error("User is not admin");
+    }
+
+    const all = this.usersRepository.list();
+
+    return all;
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,10 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    // this.usersRepository.turnAdmin({ user_id });
+    const user = this.usersRepository.findById(user_id);
+
+    return this.usersRepository.turnAdmin(user);
   }
 }
 
